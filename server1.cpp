107bd3c5c2887c6658934ed994d5aa6dcd829ed4
Ryan on Ubuntu <yulinye.yy@gmail.com>
2013-11-03 00:28:36 -0700

    code cleaned

--- server1.cpp
+++ server1.cpp
@@ -19,30 +19,19 @@
 
 using namespace std;
 
-#define PORT "3490"  // the port users will be connecting to
-
-#define BACKLOG 10     // how many pending connections queue will hold
-
-#define filename "output.txt"
-
-
-int new_fd;
-
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa);
-
+const char PORT[]= "3490";  // the port users will be connecting to
+const int BACKLOG=10;     // how many pending connections queue will hold
+long serviceCount=0;
 
 void* serve_it(void*);
 
-
 int main(void)
 {
-    int sockfd;  // listen on sock_fd, new connection on new_fd
+    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
     struct addrinfo hints, *servinfo, *p;
     struct sockaddr_storage their_addr; // connector's address information
     socklen_t sin_size;
     int yes=1;
-    char s[INET6_ADDRSTRLEN];
     int rv;
 
 
@@ -52,7 +41,7 @@ int main(void)
     hints.ai_flags = AI_PASSIVE; // use my IP
 
     if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    	fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
@@ -60,19 +49,19 @@ int main(void)
     for(p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
                 p->ai_protocol)) == -1) {
-            perror("server: socket");
+        	cerr<<"error in creating socket."<<endl;
             continue;
         }
 
         if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
                 sizeof(int)) == -1) {
-            perror("setsockopt");
+        	cerr<<"error in setsockopt."<<endl;
             exit(1);
         }
 
         if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
             close(sockfd);
-            perror("server: bind");
+            cerr<<"error in biding."<<endl;
             continue;
         }
 
@@ -80,47 +69,35 @@ int main(void)
     }
 
     if (p == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
+        cerr<<"server: failed to bind"<<endl;
         return 2;
     }
 
     freeaddrinfo(servinfo); // all done with this structure
 
     if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
+        cerr<<"error in listening"<<endl;
         exit(1);
     }
-
-
 
     pthread_t t_id;
     pthread_attr_t attr;
     pthread_attr_init(&attr); // Creating thread attributes
     pthread_attr_setschedpolicy(&attr, SCHED_FIFO); // FIFO scheduling for threads
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED); // Don't want threads (particualrly main)
-                                                                 // waiting on each other
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED); // Don't want threads  waiting on each other
 
-    printf("server: waiting for connections...\n");
 
     while(1) {  // main accept() loop
+    	 cout<<"server: waiting for connections...\n";
+
         sin_size = sizeof their_addr;
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        cout<<"accepted!"<<endl;
         if (new_fd == -1) {
-            perror("accept");
+        	cerr<<"error in accept"<<endl;
             continue;
         }
 
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
-        printf("server: got connection from %s\n", s);
-
-
-        //reserve client socket on behalf of child thread
-
-        int n=pthread_create(&t_id, &attr, &serve_it,(void*)new_fd);
-        cout<<"thread number: "<<n<<endl;
+        pthread_create(&t_id, &attr, &serve_it,(void*)new_fd); //create a new thread to handle the connection
         sleep(0); // Giving threads some CPU time
 
     }//while
@@ -129,56 +106,46 @@ int main(void)
 }
 
 
-// get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-
 void* serve_it(void* arg)
 {
+	int csocket_df = (int) (arg);
 	string receive;
  	wordCount wc=wordCount();
-
- 	int Client = (int) (arg);
     char buf[256];
- 	bzero(buf,256);
+ 	bzero(buf,sizeof(buf));
 
-
-
-	printf("Server %d: accepted = %d\n", getpid(), Client);
-
-	if (new_fd < 0) 
+	if (csocket_df < 0)
 	{ 
-       	 	printf("Accept error on server\n");
-       		printf("ERROR on accept"); 
-        	exit(1);
+		cout<<"Accept error on server"<<endl;
+        exit(1);
 	}
-
- 	printf("Connection %d accepted\n", new_fd);
+	cout<<"Connection "<<csocket_df<<" accepted"<<endl;
 
 	while(1){
-		bzero(buf,256);
-		int bytes_receive = recv(Client,buf,sizeof(buf),0);
+		bzero(buf,sizeof(buf));
+		int bytes_receive = recv(csocket_df,buf,sizeof(buf),0);
 		if (bytes_receive == 0)
+		{
+			cout<<"0 byte meet. Finish reading!"<<endl;
 			break;
+		}
 
-		printf("%d\n", bytes_receive);
+		cout<<bytes_receive<<"  bytes received"<<endl;
 
 		if (bytes_receive < 0)
 		{
-			printf("ERROR reading from socket\n");
 			exit(1);
 		}
-
-		receive+=buf;
+		receive+=buf; //append content from buffer to string variable
 	}
-	close(Client);
+
+	cout<<"Reading done. "<<endl;
+
+	if(receive.length()==0){ //no string was received from this connection.
+		cout<<"Empty input."<<endl;
+		close(csocket_df);
+		pthread_exit(0);
+	}
 
 	wc.setStr(receive);
 	//wc.printFWord();
@@ -192,6 +159,20 @@ void* serve_it(void* arg)
 	cout<<"number of sentences:";
 	cout<<wc.getNSentence()<<endl;
 
+
+	int n = send(csocket_df,"I got your message",18,0);
+    if (n < 0)
+    {
+    	cout<<"n: "<<n<<endl;
+        perror("ERROR writing to socket");
+        exit(1);
+    }
+
+
+	serviceCount++;
+	cout<<serviceCount<<" client has been served."<<endl;
+
+	close(csocket_df); //close the client socket file description
 	pthread_exit(0);
 }
 
